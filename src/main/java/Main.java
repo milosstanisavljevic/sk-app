282fedcbc125ceae41c3bc3a9d8bd85a3af8bfe4
Miljana <mstanic5819rn@raf.rs>
2021-11-02 23:44:34 +0100

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-import javax.swing.plaf.multi.MultiSeparatorUI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main{
+
 
     public static void main(String[] args) {
         String username = null;
@@ -18,7 +18,7 @@ public class Main{
 
         try {
 
-            System.out.println("Da biste pokrenuli program ukucajte komandu mkdir zajedno za putanjom\nna kojoj zelite " +
+            System.out.println("\nDa biste pokrenuli program ukucajte komandu mkdir zajedno za putanjom\nna kojoj zelite " +
                     "da napravite skladiste i imenom skladista");
 
 
@@ -42,13 +42,13 @@ public class Main{
 
                         if(connectUser(path, impl)) {
                             connected = true;
-                            System.out.println("Uspesno ste se konektovali na skladiste, izaberite sledece opcije\n" +
+                            System.out.println("\nUspesno ste se konektovali na skladiste, izaberite sledece opcije\n" +
                                     "1. Manipulacija skladistem komanda: -mnp\n" +
                                     "2. Izmeni konfiguraciju skladista komanda: -cnfgEdit parameteri: arg0 (size) arg1 (filetype) arg2 (maxFiles)\n" +
                                     "3. Dodaj korisnika komanda: -addUser\n" +
                                     "4. Izadjite iz programa komanda: dc");
                         }else{
-                            System.out.println("Nemate pristup putanji ili niste uspesno uneli kredencijale");
+                            System.out.println("\nNemate pristup putanji ili niste uspesno uneli kredencijale");
                         }
 
                     }else {
@@ -62,7 +62,8 @@ public class Main{
                         break;
 
                     case("-cnfgEdit"):
-                        System.out.println("config");
+                        impl.updateConfig(path, list.get(1), list.get(2), list.get(3));
+                        System.out.println(path + list.get(1) + list.get(2) + list.get(3));
                         break;
 
                     case("-addUser"):
@@ -88,7 +89,7 @@ public class Main{
 
     public static boolean connectUser(String path, SpecifikacijaSkladista impl){
 
-        System.out.println("Unesite username i password kako biste se ulogovali na vase skladiste");
+        System.out.println("\nUnesite username i password kako biste se ulogovali na vase skladiste");
 
         String username = null;
         String password = null;
@@ -122,7 +123,7 @@ public class Main{
     }
 
     public static void connectSuperUser(String path, SpecifikacijaSkladista impl) {
-        System.out.println("Unesite username i password kako biste se ulogovali na vase skladiste");
+        System.out.println("\nUnesite username i password kako biste se ulogovali na vase skladiste");
 
         String username = null;
         String password = null;
@@ -145,7 +146,7 @@ public class Main{
         }
 
         impl.createRoot(path, username, password);
-        System.out.println("Vase skladiste je uspesno napravljeno, izaberite sledece opcije\n" +
+        System.out.println("\nVase skladiste je uspesno napravljeno, izaberite sledece opcije\n" +
                 "1. Manipulacija skladistem komanda: -mnp\n" +
                 "2. Izmeni konfiguraciju skladista komanda: -cnfgEdit parameteri: arg0 (size) arg1 (filetype) arg2 (maxFiles)\n" +
                 "3. Dodaj korisnika komanda: -addUser\n" +
