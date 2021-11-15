8505b1e70b63b4fcb58a99de8613a2d904c153be
Miljana <mstanic5819rn@raf.rs>
2021-11-15 21:24:25 +0100

    update8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main{
         boolean connected = false;
         SpecifikacijaSkladista impl;
         Scanner s = new Scanner(System.in);
-        List<Object>list = new ArrayList<>();
+        List<String>list = new ArrayList<>();
         //String argument = args[0];
 
 
@@ -36,13 +36,12 @@ public class Main{
                     list.add(ss);
                 }
 
-                if(list.get(0).toString().equalsIgnoreCase("mkdir")) {
+                if(list.get(0).equalsIgnoreCase("mkdir")) {
                     path = list.get(1) + "\\" + list.get(2);
 
                     if(impl.checkIfRootExists(path)){
                         impl.loadUsers(path);
                         if(connectUser(path, impl)) {
-                            connected = true;
                             System.out.println("\nUspesno ste se konektovali na skladiste, izaberite sledece opcije\n" +
                                     "1. Manipulacija skladistem komanda: -mnp\n" +
                                     "2. Izmeni konfiguraciju skladista komanda: -cnfgEdit parameteri: arg0 (size) arg1 (filetype) arg2 (maxFiles)\n" +
@@ -59,13 +58,13 @@ public class Main{
                     }
                 }
 
-                switch (list.get(0).toString()){
+                switch (list.get(0)){
                     case ("-mnp"):
                         fileManipulation(path, impl);
                         break;
 
                     case("-cnfgEdit"):
-                        if(impl.updateConfig(path, Integer.parseInt(list.get(1).toString()), (String) list.get(2), Integer.parseInt(list.get(3).toString()))){
+                        if(impl.updateConfig(path, Integer.parseInt(list.get(1)), list.get(2), Integer.parseInt(list.get(3)))){
                             System.out.println("Uspesno ste promenili konfiguraciju sa kredencijalima: " + path + list.get(1) + list.get(2) + list.get(3));
                         }else {
                             System.out.println("Nemate privilegiju za promenu konfiguracije");
@@ -81,7 +80,7 @@ public class Main{
                         break;
 
                     case("-cd"):
-                        path = path + "\\" + list.get(1).toString();
+                        path = path + "\\" + list.get(1);
                         System.out.println("\nUspesno ste se usli unutar foldera " + list.get(1).toString() + " izaberite sledece opcije\n" +
                                 "1. Manipulacija skladistem komanda: -mnp\n" +
                                 "2. Izmeni konfiguraciju skladista komanda: -cnfgEdit parameteri: arg0 (size) arg1 (filetype) arg2 (maxFiles)\n" +
@@ -93,7 +92,7 @@ public class Main{
 
                 }
 
-                if (list.get(0).toString().equalsIgnoreCase("exit")){
+                if (list.get(0).equalsIgnoreCase("exit")){
                     break;
                 }
 
@@ -264,9 +263,9 @@ public class Main{
                  "-Ukoliko zelite da dodate vise foldera ukucajte komandu: -add -folders argument:  numberOfFolders\n\n" +
                  "******\n-Ukoliko zelite da obrisete folder ukucajte komandu: -delete -folder argument: foldername\n" +
                  "-Ukoliko zelite da obrisete fajl ukucajte komandu: -delete -file argument: filename\n\n" +
-                 "******\n-Ukoliko zelite da premestite fajl iz odredjenog foldera u drugi unesite komandu: -move argument: izKogFoldera uKojiFolder fileName\n" +
+                 "******\n-Ukoliko zelite da premestite fajl iz odredjenog foldera u drugi unesite komandu: -move argument: putanjaizKogFoldera putanjauKojiFolder fileName\n" +
                  "-Ukoliko zelite da skinete/download-ujete odredjeni fajl ukucajte komadnu: -download argument: putanja imeFajla\n" +
-                 "-Ukoliko zelite da kopirate fajl iz odredjenog foldera u drugi unesite komandu: -copy argument: izKogFoldera uKojiFolder fileName\n");
+                 "-Ukoliko zelite da kopirate fajl iz odredjenog foldera u drugi unesite komandu: -copy argument: putanjaizKogFoldera putanjauKojiFolder fileName\n");
 
          List<String> list;
          Scanner s1 = new Scanner(System.in);
@@ -287,9 +286,12 @@ public class Main{
                  System.out.println("Ovaj tip fajla nije podrzan");
              }else{
                  filename = list.get(2);
-                 impl.createFile(path, filename + filetype);
+                 if(impl.createFile(path, filename + filetype)){
+                     System.out.println("Uspesno ste dodali file, izasli ste iz programa za manipulaciju");
+                 }else{
+                     System.out.println("Nemate privilegije");
+                 }
              }
-             System.out.println("Izasli ste iz programa za manipulaciju");
              return;
          }
 
@@ -308,9 +310,13 @@ public class Main{
 
          if(list.get(0).equalsIgnoreCase("-add") && list.get(1).equalsIgnoreCase("-folder") && list.size() == 3){
              foldername = list.get(2);
-             impl.createFolder(path, foldername);
-             System.out.println("Izasli ste iz programa za manipulaciju");
-             return;
+             if(impl.createFolder(path, foldername)){
+                 System.out.println("Uspesno ste dodali folder, izasli ste iz programa za manipulaciju ");
+                 return;
+             }else {
+                 System.out.println("Nemate privilegije");
+                 return;
+             }
          }
          if(list.get(0).equalsIgnoreCase("-add") && list.get(1).equalsIgnoreCase("-folders") && list.size() == 3){
              numberOfFolders = Integer.parseInt(list.get(2));
@@ -321,38 +327,54 @@ public class Main{
 
          if(list.get(0).equalsIgnoreCase("-delete") && list.get(1).equalsIgnoreCase("-folder") && list.size() == 3){
              foldername = list.get(2);
-             impl.deleteFolder(path, foldername);
-             System.out.println("Izasli ste iz programa za manipulaciju");
+             if(impl.deleteFolder(path, foldername)){
+                 System.out.println("Uspesno ste obrisali folder, izasli ste iz programa za manipulaciju ");
+             }else{
+                 System.out.println("Nemate privilegije");
+             }
              return;
          }
 
          if(list.get(0).equalsIgnoreCase("-delete") && list.get(1).equalsIgnoreCase("-file") && list.size() == 3){
              filename = list.get(2);
-             impl.deleteFile(path, filename);
-             System.out.println("Izasli ste iz programa za manipulaciju");
+             if(impl.deleteFile(path, filename)){
+                 System.out.println("Uspesno ste obrisali fajl, izasli ste iz programa za manipulaciju ");
+
+             }else{
+                 System.out.println("Nemate privilegije");
+
+             }
              return;
          }
 
          if(list.get(0).equalsIgnoreCase("-move") && list.size() == 4){
-             impl.moveFromTo(list.get(1), list.get(2), list.get(3));
-             System.out.println("Izasli ste iz programa za manipulaciju");
+             if(impl.moveFromTo(list.get(1), list.get(2), list.get(3))){
+                 System.out.println("Uspesno ste premestili fajl, izasli ste iz programa za manipulaciju ");
+             }else{
+                 System.out.println("Nemate privilegije");
+             }
              return;
          }
 
          if(list.get(0).equalsIgnoreCase("-copy") && list.size() == 4){
-             impl.copyPasteFiles(list.get(1), list.get(2), list.get(3));
-             System.out.println("Izasli ste iz programa za manipulaciju");
+             if(impl.copyPasteFiles(list.get(1), list.get(2), list.get(3))){
+                 System.out.println("Uspesno ste skinuli fajl, izasli ste iz programa za manipulaciju ");
+             }else{
+                 System.out.println("Nemate privilegije");
+             }
              return;
          }
 
          if(list.get(0).equalsIgnoreCase("-download") && list.size() == 3){
-             impl.downloadFile(list.get(1), list.get(2));
-             System.out.println("Izasli ste iz programa za manipulaciju");
+             if(impl.downloadFile(list.get(1), list.get(2))){
+                 System.out.println("Uspesno ste kopirali fajl, izasli ste iz programa za manipulaciju ");
+             }else{
+                 System.out.println("Nemate privilegije");
+             }
              return;
          }
 
          System.out.println("Izasli ste iz programa za manipulaciju");
-         return;
 
 
      }
